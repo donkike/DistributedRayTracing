5e11292e9e81f2a04dbdb037ded7cad94225b8c3
Esteban Alarcon Ceballos <nocrala.nabetse@gmail.com>
2011-05-23 16:07:02 -0500

    initial reflection and refraction

--- src/raytracing/Main.java
+++ src/raytracing/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scene scene = new Scene(600, 400);
-		scene.addObject(new Sphere(new Vector(200.0, 200.0, 100.0), 100.0, new Material(Color.ORANGE, 0.0, 0.0)));
+		scene.addObject(new Sphere(new Vector(200.0, 200.0, 100.0), 100.0, new Material(Color.ORANGE, 0.6, 0.8)));
 		scene.addObject(new Plane(new Vector(0.0, 0.0, -1.0), 200.0, new Material(Color.CYAN, 0.0, 0.0)));
 		
 		RayTracer rt = new RayTracer(scene);
